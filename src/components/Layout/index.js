42cb3a06106880f4d806f3a1b81b13618bc0cead
Kostia Lub <konstantin.lubinskiy@techcoresoft.com>
2021-09-08 17:42:36 +0300

    add homework 1

---
+++ src/components/Layout/index.js
@@ -0,0 +1,23 @@
+import style from './style.module.css';
+console.log(style);
+
+const Layout = ({ title, descr, urlBg, colorBg = 'yellow' }) => {
+    const styleRoot = urlBg ? { background: `url(${urlBg})` } : { background: `${colorBg}`};
+    return (
+        <section className={ style.root } style={ styleRoot }>
+            <div className={ style.wrapper }>
+                <article>
+                    <div className={ style.title }>
+                        <h3>{ title }</h3>
+                        <span className={ style.separator }></span>
+                    </div>
+                    <div className={`{${style.desc}, ${style.full}}`}>
+                        <p>{ descr }</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    )
+}
+
+export default Layout;
