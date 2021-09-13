0fb11ce8b2d0c2705515994adb1fe46238d52326
Kostia Lub <konstantin.lubinskiy@techcoresoft.com>
2021-09-13 11:54:26 +0300

    fix open/close cards, move POKEMONS to pokemons.js

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -7,7 +7,7 @@ const PokemonCard = ({name, img, id, type, values}) => {
     const [isActive, setActive] = useState(false);
 
     const handleClick = () => {
-        setActive(true);
+        isActive ? setActive(false) : setActive (true);
     }
 
     return (
